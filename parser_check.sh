404433d9b755681007c43ade4211fca146974782
josh! <josh.highet@theta.co.nz>
2022-03-31 07:56:27 +1300

    Update parser_check.sh

--- parser_check.sh
+++ parser_check.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+# used to check each parser is returning findings
+# run with -p to print the entailing shellcode
 
 shell_parsers=`grep -A 2 "parser = '''" parsers.py | sed -e "/parser = '''/d" -e "/'''/d" -e '/^--$/d'`
 
+if [ "${1}" == "-p" ]; then
+    while read -r line; do
+        if [[ ${line} =~ %s ]]; then
+            line=${line//%s/grep -oE}
+        fi
+        echo ${line}
+    done <<< "${shell_parsers}"
+    exit 0
+fi
+
 while read -r line; do
-    # if line contains %s replace it with "grep -oE"
-    if [[ $line =~ %s ]]; then
+    if [[ ${line} =~ %s ]]; then
         line=${line//%s/grep -oE}
     fi
-    results=`echo "$line" | bash`
-    if [ -z "$results" ]; then
+    results=`echo "${line}" | bash`
+    if [ -z "${results}" ]; then
         echo "error: broken"
-        echo "$line"
+        echo "${line}"
         echo ""
     fi
 done <<< "$shell_parsers"
