33e35bf7c9fa4f77452a1596e6df8789a6a1889c
josh! <josh.highet@theta.co.nz>
2022-04-29 16:58:56 +1200

    simp fetch

--- assets/sources.zsh
+++ assets/sources.zsh
@@ -7,30 +7,19 @@ if ! `nc -z localhost 9050`; then
     exit 1
 fi
 
-echo
-
-exclusions=`cat assets/sources.exclusions`
-total_exclusions=`echo ${exclusions} | wc -w | sed -E -e 's/^[[:space:]]*//'`
-
 random_useragent=`cat assets/useragents.txt | shuf -n 1`
 
-ransomwatch_groups=`jq '.[].locations[].fqdn' groups.json -r`
-if [ $? -ne 0 ]; then
-    echo "failed to read file ransomwatch/groups.json"
-    exit 1
+if [ ! -d assets/tmp ]; then
+    mkdir assets/tmp
 fi
-ransomwatch_groups_count=`echo $ransomwatch_groups | wc -w | tr -d ' '`
-echo "tracking ${ransomwatch_groups_count} hosts | local:groups.json"
-echo "excluding ${total_exclusions} hosts | local:assets/fetch.exclusions"
-echo
 
 googlesheCZJ=`curl -s 'https://docs.google.com/spreadsheets/d/1cH4KCZJvggoHPAbk0u08Wu1vSo9ygx47QfhKD-W0TQ0/gviz/tq?tqx=out:csv' -H 'User-Agent: '${random_useragent}''`
 if [ $? -ne 0 ]; then
     echo "failed to fetch from googlesheets:1cH4KCZJ"
     exit 1
 fi
-googlesheCZJ_parse=`echo $googlesheCZJ | cut -d ',' -f 3 | grep onion | sed -E -e 's/^[[:space:]]*//' -e 's/^"//' -e 's/"$//' -e 's:/*$::' -e 's/onion.ly/onion/g' -e 's/http:\/\///' -e 's/https:\/\///' | cut -f1 -d"/"`
-googlesheCZJ_count=`echo ${googlesheCZJ_parse} | wc -w | sed -E -e 's/^[[:space:]]*//'`
+echo $googlesheCZJ | cut -d ',' -f 3 | grep onion | sed -E -e 's/^[[:space:]]*//' -e 's/^"//' -e 's/"$//' -e 's:/*$::' -e 's/onion.ly/onion/g' -e 's/http:\/\///' -e 's/https:\/\///' | cut -f1 -d"/" > assets/tmp/sources.googlesheCZJ
+googlesheCZJ_count=`cat assets/tmp/sources.googlesheCZJ | wc -w | awk '{$1=$1};1'`
 echo "${googlesheCZJ_count} | googlesheets:1cH4KCZJ"
 
 rgs=`curl -s --socks5-hostname localhost:9050 ransomwr3tsydeii4q43vazm7wofla5ujdajquitomtd47cxjtfgwyyd.onion -H 'User-Agent: '${random_useragent}''`
@@ -38,27 +27,17 @@ if [ $? -ne 0 ]; then
     echo "failed to fetch from onion:ransomwr3"
     exit 1
 fi
-rgs_parse=`echo ${rgs} | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' | grep onion | cut -d '"' -f 1`
-rgs_count=`echo ${rgs_parse} | wc -w | sed -E -e 's/^[[:space:]]+//' -e 's/[[:space:]]+$//'`
+echo ${rgs} | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' | grep onion | cut -d '"' -f 1 > assets/tmp/sources.rgs
+rgs_count=`cat assets/tmp/sources.rgs | wc -w | awk '{$1=$1};1'`
 echo "${rgs_count} | onion:ransomwr3"
-
-# echo "fetching from clearnet:darkfeedio"
-# dkfeed=`curl -s  -H 'User-Agent: '${random_useragent}'' https://darkfeed.io/ransomwiki/`
-# if [ $? -ne 0 ]; then
-#     echo "failed to fetch from clearnet:darkfeedio"
-#     exit 1
-# fi
-# dkfeed_parse=`echo ${dkfeed} | grep '</td><td class="column-2">TOR</td><td class="column-3"><a href="' | cut -d '"' -f 8 | sed -e 's/.onion.ly/.onion/g' -e 's/.onion.ws/.onion/g' -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_'`
-# dkfeed_count=`echo ${dkfeed_parse} | wc -w | sed -E -e 's/^[[:space:]]+//' -e 's/[[:space:]]+$//'`
-# echo "${dkfeed_count}"
 
 ddcti_ransomgang=`curl -s https://raw.githubusercontent.com/fastfire/deepdarkCTI/main/ransomware_gang.md  -H 'User-Agent: '${random_useragent}''`
 if [ $? -ne 0 ]; then
     echo "failed to fetch from github:deepdarkCTI:ransomgang"
     exit 1
 fi
-ddcti_ransomgang_parse=`echo ${ddcti_ransomgang} | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' | grep onion | cut -d ' ' -f 1 | cut -d '|' -f 1`
-ddcti_ransomgang_count=`echo ${ddcti_ransomgang_parse} | wc -w | sed -E -e 's/^[[:space:]]*//'`
+echo ${ddcti_ransomgang} | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' | grep onion | cut -d ' ' -f 1 | cut -d '|' -f 1 > assets/tmp/sources.ddcti_ransomgang
+ddcti_ransomgang_count=`cat assets/tmp/sources.ddcti_ransomgang | wc -w | awk '{$1=$1};1'`
 echo "${ddcti_ransomgang_count} | github:deepdarkCTI:ransomgang"
 
 ddcti_maas=`curl -s https://raw.githubusercontent.com/fastfire/deepdarkCTI/main/maas.md -H 'User-Agent: '${random_useragent}''`
@@ -66,8 +45,8 @@ if [ $? -ne 0 ]; then
     echo "failed to fetch from github:deepdarkCTI:maas"
     exit 1
 fi
-ddcti_maas_parse=`echo ${ddcti_maas} | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' | grep onion | cut -d ' ' -f 1 | cut -d '|' -f 1`
-ddcti_maas_count=`echo ${ddcti_maas_parse} | wc -w | sed -E -e 's/^[[:space:]]*//'`
+echo ${ddcti_maas} | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' | grep onion | cut -d ' ' -f 1 | cut -d '|' -f 1 > assets/tmp/sources.ddcti_maas
+ddcti_maas_count=`cat assets/tmp/sources.ddcti_maas | wc -w | awk '{$1=$1};1'`
 echo "${ddcti_maas_count} | github:deepdarkCTI:maas"
 
 ddcti_markets=`curl -s https://raw.githubusercontent.com/fastfire/deepdarkCTI/main/markets.md -H 'User-Agent: '${random_useragent}''`
@@ -75,8 +54,8 @@ if [ $? -ne 0 ]; then
     echo "failed to fetch from github:deepdarkCTI:markets"
     exit 1
 fi
-ddcti_markets_parse=`echo ${ddcti_markets} | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' | grep onion | cut -d ' ' -f 1 | cut -d '|' -f 1`
-ddcti_markets_count=`echo ${ddcti_markets_parse} | wc -w | sed -E -e 's/^[[:space:]]*//'`
+echo ${ddcti_markets} | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' | grep onion | cut -d ' ' -f 1 | cut -d '|' -f 1 > assets/tmp/sources.ddcti_markets
+ddcti_markets_count=`cat assets/tmp/sources.ddcti_markets | wc -w | awk '{$1=$1};1'`
 echo "${ddcti_markets_count} | github:deepdarkCTI:markets"
 
 ddcti_forum=`curl -s https://raw.githubusercontent.com/fastfire/deepdarkCTI/main/forum.md -H 'User-Agent: '${random_useragent}''`
@@ -84,8 +63,8 @@ if [ $? -ne 0 ]; then
     echo "failed to fetch from github:deepdarkCTI:forum"
     exit 1
 fi
-ddcti_forum_parse=`echo ${ddcti_forum} | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' | grep onion | cut -d ' ' -f 1 | cut -d '|' -f 1`
-ddcti_forum_count=`echo ${ddcti_forum_parse} | wc -w | sed -E -e 's/^[[:space:]]*//'`
+echo ${ddcti_forum} | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' | grep onion | cut -d ' ' -f 1 | cut -d '|' -f 1 > assets/tmp/sources.ddcti_forum
+ddcti_forum_count=`cat assets/tmp/sources.ddcti_forum | wc -w | awk '{$1=$1};1'`
 echo "${ddcti_forum_count} | github:deepdarkCTI:forum"
 
 gistteix=`curl -s https://gist.githubusercontent.com/teixeira0xfffff/3ac8d9c4bf113e56533299b2da8c856b/raw/4e70ff99a7af2a86f720de65d5218f7b9c9f21d0/ransomwarefeed.csv -H 'User-Agent: '${random_useragent}''`
@@ -93,28 +72,40 @@ if [ $? -ne 0 ]; then
     echo "failed to fetch from gist:teixeira0xfffff:ransomwarefeed.csv"
     exit 1
 fi
-gistteix_parse=`echo ${gistteix} | cut -d ',' -f 2 | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_'`
-gistteix_count=`echo ${gistteix_parse} | wc -w | sed -E -e 's/^[[:space:]]*//'`
+echo ${gistteix} | cut -d ',' -f 2 | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' > assets/tmp/sources.gistteix
+gistteix_count=`cat assets/tmp/sources.gistteix | wc -w | awk '{$1=$1};1'`
 echo "${gistteix_count} | gist:teixeira0xfffff:ransomwarefeed.csv"
 
-hosts=`echo "${googlesheCZJ_parse} ${gistteix_parse} ${rgs_parse} ${ddcti_ransomgang_parse} ${ddcti_maas_parse} ${ddcti_markets_parse} ${ddcti_forum_parse} ${dkfeed_parse}" | tr ' ' '\n' | sort -u | tr '[:upper:]' '[:lower:]'`
-hosts=`echo "${hosts}" | grep -v -E "${exclusions}"`
-total_unique_hosts=`echo "${hosts}" | wc -w | sed -E -e 's/^[[:space:]]*//'`
-echo
-echo "${total_unique_hosts}  | merge-unique count"
-hosts=`echo "${hosts}" | sed -E -e '/^[[:space:]]*$/d' -e 's/url//'`
-found=False
+freshoni=`curl -s --socks5-hostname localhost:9050 'freshonifyfe4rmuh6qwpsexfhdrww7wnt5qmkoertwxmcuvm4woo4ad.onion/?query=ransom' -H 'User-Agent: '${random_useragent}''`
+if [ $? -ne 0 ]; then
+    echo "failed to fetch from onion:freshonify"
+    exit 1
+fi
+echo ${freshoni} | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' | grep onion | cut -d '"' -f 1 > assets/tmp/sources.freshoni
+freshoni_count=`cat assets/tmp/sources.freshoni | wc -w | awk '{$1=$1};1'`
+echo "${freshoni_count} | onion:freshonify"
 
-echo
+lpn=`curl -s --socks5-hostname localhost:9050 lpnxgtkni46pngdg4pml47hvxg2xqdcrd7z2f5oysyuialodho6g34yd.onion -H 'User-Agent: '${random_useragent}''`
+if [ $? -ne 0 ]; then
+    echo "failed to fetch from onion:lpnxgtkni"
+    exit 1
+fi
+echo ${lpn} | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' | grep onion | cut -d '"' -f 1 > assets/tmp/sources.lpn
+lpn_count=`cat assets/tmp/sources.lpn | wc -w | awk '{$1=$1};1'`
+echo "${lpn_count} | onion:lpnxgtkni"
 
-while read -r host; do
-    if ! echo "${ransomwatch_groups}" | grep -q "${host}"; then
-        found=True
+inh=`curl -s --socks5-hostname localhost:9050 inhx4x4y66guy6ljnhq3ijbbgroha5sejcyo2uejmzv6vd3ydwzc6fid.onion -H 'User-Agent: '${random_useragent}''`
+if [ $? -ne 0 ]; then
+    echo "failed to fetch from onion:inhx4x4y6"
+    exit 1
+fi
+echo ${inh} | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' | grep onion | cut -d '"' -f 1 > assets/tmp/sources.inh
+inh_count=`cat assets/tmp/sources.inh | wc -w | awk '{$1=$1};1'`
+echo "${inh_count} | onion:inhx4x4y6"
+
+cat assets/tmp/sources.* | sort | uniq | tr '[:upper:]' '[:lower:]' | grep -Eo '^([a-z0-9][a-z0-9_-]*\.)*[a-z2-7]{56}\.onion' | while read host;
+do
+    if ! grep -q "${host}" assets/sources.exclusions && ! grep -q "${host}" groups.json; then
         echo "${host}"
-        echo "${ddcti_ransomgang} ${ddcti_maas} ${ddcti_markets} ${ddcti_forum} ${dkfeed} ${rgs}" | grep "${host}"
     fi
 done <<< "${hosts}"
-
-if [ "${found}" = "False" ]; then
-    echo "no untracked hosts found"
-fi
